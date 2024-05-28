ee1e4c6f756e9fb635ac9b6e0ebec68b389f093c
syoker <lautarivan@hotmail.com>
2024-05-28 14:22:01 -0300

    fixed indent

--- main.py
+++ main.py
@@ -50,80 +50,80 @@ def get_unit(n, units):
     return 'pass'
 
 def main():
-  units = ['Basics',
-  'Sequential Structures',
-  'Basic Structured Types',
-  'Conditional Structures',
-  'Conditional Structures: Variants',
-  'Repetitive Structures: While Loop',
-  'Repetitive Structures: For Loop',
-  'Repetitive Structures: Variants',
-  'Character Sequence Processing',
-  'Subproblems and Functions',
-  'Functions: Detailed Implementation']
+    units = ['Basics',
+    'Sequential Structures',
+    'Basic Structured Types',
+    'Conditional Structures',
+    'Conditional Structures: Variants',
+    'Repetitive Structures: While Loop',
+    'Repetitive Structures: For Loop',
+    'Repetitive Structures: Variants',
+    'Character Sequence Processing',
+    'Subproblems and Functions',
+    'Functions: Detailed Implementation']
 
-  while True:
-    while_condition = 1
-    counter = 0
+    while True:
+        while_condition = 1
+        counter = 0
 
-    clear_history()
-    print('Algorithms and Data Structures')
-    print('')
-    print('00 - Exit')
-    for unit in units:
-      counter += 1
-      print(f'{counter:02d} - {unit}')
-    print('')
-    
-    unit_written = input('Select a unit to open: ')
-    unit_processed = get_unit(unit_written, units)
-
-    if unit_processed == 'exit':
-        break
-    elif unit_processed == 'pass':
-        while_condition = 0
-        
-    while while_condition == 1:
         clear_history()
-
-        print(unit_processed[1])
-        file_selected = None
-        unit_files = get_list(unit_processed[0])
-        
+        print('Algorithms and Data Structures')
         print('')
-        print('00. Back')
-        for unit_file in unit_files:
-            print(unit_file)
+        print('00 - Exit')
+        for unit in units:
+            counter += 1
+            print(f'{counter:02d} - {unit}')
         print('')
+    
+        unit_written = input('Select a unit to open: ')
+        unit_processed = get_unit(unit_written, units)
 
-        filename_written = input('Select a file to execute: ')
-        print('')
-
-        if filename_written in ['00 - Back', 'Back', 'back', '00', '0']:
+        if unit_processed == 'exit':
             break
+        elif unit_processed == 'pass':
+            while_condition = 0
+        
+        while while_condition == 1:
+            clear_history()
 
-        for unit_file in unit_files:
-            options = []
-            options.append(unit_file)
-            options.append(unit_file[4:])
-            options.append(unit_file[4:-3])
-            options.append(unit_file[0:2])
-            if unit_file[0:2] in ['01', '02', '03', '04', '05', '06', '07', '08', '09']:
-                options.append(unit_file[1])
+            print(unit_processed[1])
+            file_selected = None
+            unit_files = get_list(unit_processed[0])
+        
+            print('')
+            print('00. Back')
+            for unit_file in unit_files:
+                print(unit_file)
+            print('')
 
-            if filename_written in options:
-                file_selected = unit_file[4:-3]
+            filename_written = input('Select a file to execute: ')
+            print('')
+
+            if filename_written in ['00 - Back', 'Back', 'back', '00', '0']:
                 break
 
-        if file_selected != None:
-            try:
-                function_main = get_function(unit_processed[0], file_selected).main
-                function_main()
-            except ValueError as error:
+            for unit_file in unit_files:
+                options = []
+                options.append(unit_file)
+                options.append(unit_file[4:])
+                options.append(unit_file[4:-3])
+                options.append(unit_file[0:2])
+                if unit_file[0:2] in ['01', '02', '03', '04', '05', '06', '07', '08', '09']:
+                    options.append(unit_file[1])
+
+                if filename_written in options:
+                    file_selected = unit_file[4:-3]
+                    break
+
+            if file_selected != None:
+                try:
+                    function_main = get_function(unit_processed[0], file_selected).main
+                    function_main()
+                except ValueError as error:
+                    print('')
+                    print(error)
                 print('')
-                print(error)
-            print('')
-            input('Continue...')
+                input('Continue...')
         
 
 if __name__ == '__main__':
